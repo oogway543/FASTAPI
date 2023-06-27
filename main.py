ea6b340b2ccc3d14834f8e42b83325c64e9ee98a
oogway <oogway543543@gmail.com>
2023-06-27 06:33:16 +0000

    0

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return{"item_id": item_id}
+
+#query parameter
+@app.get_("/raspberry/")
+async def read_item(time:datetime = datetime.now(),light: flat = 0.0, temerature: flat = 0.0):
+    return{
+        "時間":time,
+        "光線":light,
+        "溫度":temperature
+    }
+
